e2ade57517682857b737079adb05695cecafe857
featattach <featattach@gmail.com>
2024-01-06 20:37:57 +0500

    Laba 7 pre final

---
+++ src/main/java/lvov/lab7/controller/StudentController.java
@@ -0,0 +1,54 @@
+package lvov.lab7.controller;
+import lombok.extern.slf4j.Slf4j;
+import lvov.lab7.dao.StudentRepository;
+import lvov.lab7.entity.Student;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.Banner;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.view.RedirectView;
+
+import java.util.Optional;
+@Slf4j
+@RestController
+public class StudentController {
+    private final StudentRepository studentRepository;
+    @Autowired
+    public StudentController(StudentRepository studentRepository) {
+        this.studentRepository = studentRepository;
+    }
+    @GetMapping({"/list", "/"})
+    public ModelAndView getAllStudent() {
+        ModelAndView mav = new ModelAndView("list-students");
+        mav.addObject("students", studentRepository.findAll());
+        return mav;
+    }
+    @GetMapping("/addStudentForm")
+    public ModelAndView addStudentForm() {
+        ModelAndView mav = new ModelAndView("add-student-form");
+        Student student = new Student();
+        mav.addObject("student", student);
+        return mav;
+    }
+    @PostMapping("/saveStudent")
+    public RedirectView saveStudent(@ModelAttribute Student student) {
+        studentRepository.save(student);
+        return new RedirectView("list");
+    }
+    @PostMapping("/showUpdateForm")
+    public ModelAndView showUpdateForm(@RequestParam Long studentId) {
+        ModelAndView mav = new ModelAndView("add-student-form");
+        Optional<Student> optionalStudent = studentRepository.findById(studentId);
+        Student student = new Student();
+        if (optionalStudent.isPresent()) {
+            student = optionalStudent.get();
+        }
+        mav.addObject("student", student);
+        return mav;
+    }
+    @GetMapping("deleteStudent")
+    public RedirectView deleteStudent(@RequestParam Long studentId, ModelAndView model) {
+        studentRepository.deleteById(studentId);
+        return new RedirectView("list");
+    }
+}
---
+++ src/main/java/lvov/lab7/entity/Student.java
@@ -0,0 +1,30 @@
+package lvov.lab7.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "STUDENTS")
+
+
+public class Student {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+
+    private long id;
+    @Column(name="name")
+    private String name;
+    @Column(name = "surname")
+    private String surname;
+    @Column(name = "faculty")
+    private String faculty;
+    @Column(name = "age")
+    private int age;
+
+}
+
